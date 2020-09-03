9076bf30e82b296b9821283ca0c419277002a24a
Timothy Small <smalls.online@icloud.com>
2020-09-03 19:25:46 -0400

    Fixed profile mapping
    - Removed `ConvertFrom-Json` as it isn't needed and would throw an
      error.
    - Fixed the PSVersion matching to be `Major.Minor` instead of
      `Major.Major`.

--- InstallProfile.ps1
+++ InstallProfile.ps1
@@ -1,7 +1,5 @@
-Invoke-Command -ScriptBlock {
-    $profileMapping = Invoke-RestMethod -Method "Get" -Uri "https://raw.githubusercontent.com/Smalls1652/My-PowerShell-Profiles/master/install-deps/ProfileDownload.json" -ErrorAction "Stop" | ConvertFrom-Json
+$profileMapping = Invoke-RestMethod -Method "Get" -Uri "https://raw.githubusercontent.com/Smalls1652/My-PowerShell-Profiles/master/install-deps/ProfileDownload.json" -ErrorAction "Stop"
 
-    $profileToDownload = $profileMapping | Where-Object { ($PSItem.psVersion -eq "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Major)") -and ($PSItem.psPlatform -eq $PSVersionTable.Platform) -and ($PSItem.psEdition -eq $PSVersionTable.PSEdition) }
+$profileToDownload = $profileMapping | Where-Object { ($PSItem.psVersion -eq "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)") -and ($PSItem.psPlatform -eq $PSVersionTable.Platform) -and ($PSItem.psEdition -eq $PSVersionTable.PSEdition) }
 
-    Invoke-WebRequest -Uri $profileToDownload.profileDownloadUri -OutFile $PROFILE
-}
+Invoke-WebRequest -Uri $profileToDownload.profileDownloadUri -OutFile $PROFILE
