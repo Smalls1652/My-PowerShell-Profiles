07fe12d473d9a029ce564b37fadeb81b09d04df3
Timothy Small <smalls.online@icloud.com>
2020-09-03 19:21:25 -0400

    Fixed incorrect uri

--- InstallProfile.ps1
+++ InstallProfile.ps1
@@ -1,5 +1,5 @@
 Invoke-Command -ScriptBlock {
-    $profileMapping = Invoke-RestMethod -Method "Get" -Uri "https://raw.githubusercontent.com/Smalls1652/My-PowerShell-Profiles/master/profiles/install-deps/ProfileDownload.json" -ErrorAction "Stop" | ConvertFrom-Json
+    $profileMapping = Invoke-RestMethod -Method "Get" -Uri "https://raw.githubusercontent.com/Smalls1652/My-PowerShell-Profiles/master/install-deps/ProfileDownload.json" -ErrorAction "Stop" | ConvertFrom-Json
 
     $profileToDownload = $profileMapping | Where-Object { ($PSItem.psVersion -eq "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Major)") -and ($PSItem.psPlatform -eq $PSVersionTable.Platform) -and ($PSItem.psEdition -eq $PSVersionTable.PSEdition) }
 
