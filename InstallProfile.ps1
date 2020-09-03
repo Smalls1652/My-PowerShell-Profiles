7a399eca7ee9d978efa8fb40643055c9ed801446
Timothy Small <smalls.online@icloud.com>
2020-09-03 19:31:47 -0400

    Added comments for what each step does

--- InstallProfile.ps1
+++ InstallProfile.ps1
@@ -1,5 +1,8 @@
+#Gets the profile version mapping JSON file from the GitHub repo, on the master branch.
 $profileMapping = Invoke-RestMethod -Method "Get" -Uri "https://raw.githubusercontent.com/Smalls1652/My-PowerShell-Profiles/master/install-deps/ProfileDownload.json" -ErrorAction "Stop"
 
+#Parses the profile map based off the version, edition, and platform and gets the object suited for it.
 $profileToDownload = $profileMapping | Where-Object { ($PSItem.psVersion -eq "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)") -and ($PSItem.psPlatform -eq $PSVersionTable.Platform) -and ($PSItem.psEdition -eq $PSVersionTable.PSEdition) }
 
+#Downloads the profile
 Invoke-WebRequest -Uri $profileToDownload.profileDownloadUri -OutFile $PROFILE
