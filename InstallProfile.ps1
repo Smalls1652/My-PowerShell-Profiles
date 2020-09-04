19ae7a08f9301faebfa89fc9163c488bed041e0a
Timothy Small <smalls.online@icloud.com>
2020-09-03 20:00:53 -0400

    Fixed profile map / added profile dir check
    - Removed version check as it may cause issues with future versions of
      PowerShell 7.
    - Added profile directory check and creation if it doesn't exist.

--- InstallProfile.ps1
+++ InstallProfile.ps1
@@ -2,7 +2,12 @@
 $profileMapping = Invoke-RestMethod -Method "Get" -Uri "https://raw.githubusercontent.com/Smalls1652/My-PowerShell-Profiles/master/install-deps/ProfileDownload.json" -ErrorAction "Stop"
 
 #Parses the profile map based off the version, edition, and platform and gets the object suited for it.
-$profileToDownload = $profileMapping | Where-Object { ($PSItem.psVersion -eq "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)") -and ($PSItem.psPlatform -eq $PSVersionTable.Platform) -and ($PSItem.psEdition -eq $PSVersionTable.PSEdition) }
+$profileToDownload = $profileMapping | Where-Object { ($PSItem.psPlatform -eq $PSVersionTable.Platform) -and ($PSItem.psEdition -eq $PSVersionTable.PSEdition) }
+
+#Check to see if the directory the profile resides in actually exists. If it doesn't, create it.
+if(!([System.IO.Directory]::Exists([System.IO.Path]::GetDirectoryName($profile)))) {
+    $null = New-Item -Path ([System.IO.Path]::GetDirectoryName($profile)) -ItemType Directory -Force
+}
 
 #Downloads the profile
 Invoke-WebRequest -Uri $profileToDownload.profileDownloadUri -OutFile $PROFILE
